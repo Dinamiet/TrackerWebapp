4e37d15329532a602ff12829ea166030a42ced01
Dinamiet <iceman.sm39@gmail.com>
2025-11-21 17:05:13 +0200

    Accepting RAW encrypted data

--- src/pages/api/location.js
+++ src/pages/api/location.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-let location = {name: "", lat: 0, lng: 0};
+let location = {};
 
 export default async function handler(req, res) {
 
@@ -8,15 +8,32 @@ export default async function handler(req, res) {
 
   switch (req.method) {
     case 'POST':
-      location = req.body;
-      console.log("POST:", location, name);
-      return res.status(200).json({ message: 'Success' });
+      let rawBody = Buffer.alloc(0);
+      req.on('data', (chunk) => {
+        rawBody = Buffer.concat([rawBody, chunk]);
+      });
+
+      req.on('end', () => {
+        location[name] = rawBody;
+        res.status(200).json({ message: 'Success' });
+      });
+
+      req.on('error', (err) => {
+        res.status(500).json({ message: 'Error' });
+      });
+      return;
 
     case 'GET':
-      return res.status(200).json(location);
+      return res.status(200).json(location[name]);
 
     default:
       return res.status(503).json({message: "Not implemented"});
   }
 
 }
+
+export const config = {
+  api: {
+    bodyParser: false, // Disable Next.js body parsing
+  },
+};
